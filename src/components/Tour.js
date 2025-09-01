48c331488119576b80e4a3168bdc8fe2f0c30b86
York Yu <n11453559@gmail.com>
2025-09-02 08:53:21 +1000

    Refactor Services and Tours components to utilize Service and Tour components for rendering

---
+++ src/components/Tour.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Tour({ image, date, title, info, location, duration, price }) {
+  return (
+    <article className="tour-card">
+      <div className="tour-img-container">
+        <img src={image} className="tour-img" alt="" />
+        <p className="tour-date">{date}</p>
+      </div>
+      <div className="tour-info">
+        <div className="tour-title">
+          <h4>{title}</h4>
+        </div>
+        <p>{info}</p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map"></i>
+            </span>{' '}
+            {location}
+          </p>
+          <p>{duration}</p>
+          <p>from ${price}</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Tour
