fe2960b112a8478f7ba2a31f3c7e2a0a8ed4305d
York Yu <n11453559@gmail.com>
2025-09-01 21:56:14 +1000

    Refactor Footer and Navbar components to utilize PageLinks for rendering links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../images/logo.svg'
-import { pageLinks, socialLinks } from '../data'
+import { socialLinks } from '../data'
+import PageLinks from './PageLinks'
 
 const Navbar = () => {
   return (
@@ -12,18 +13,7 @@ const Navbar = () => {
             <i className="fas fa-bars"></i>
           </button>
         </div>
-        {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            )
-          })}
-        </ul>
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
 
         <ul className="nav-icons">
           {socialLinks.map((link) => {
