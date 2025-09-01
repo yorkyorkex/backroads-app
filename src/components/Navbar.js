e847bc832218330850eeb054467f1c1c326024df
York Yu <n11453559@gmail.com>
2025-09-01 15:55:15 +1000

    Add pageLinks data and refactor Navbar component for dynamic navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../images/logo.svg'
+import { pageLinks } from '../data'
 
 const Navbar = () => {
   return (
@@ -13,33 +14,15 @@ const Navbar = () => {
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              {' '}
-              home{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              {' '}
-              about{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {' '}
-              services{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {' '}
-              tours
-            </a>
-          </li>
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            )
+          })}
         </ul>
 
         <ul className="nav-icons">
