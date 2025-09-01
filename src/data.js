492c4d9dff0a81d3beb4165217b95d71e0605655
York Yu <n11453559@gmail.com>
2025-09-02 08:55:48 +1000

    Remove unused tour image imports from data.js

--- src/data.js
+++ src/data.js
@@ -2,8 +2,8 @@ import tour1 from './images/tour-1.jpeg'
 import tour2 from './images/tour-2.jpeg'
 import tour3 from './images/tour-3.jpeg'
 import tour4 from './images/tour-4.jpeg'
-import tour5 from './images/tour-5.jpeg'
-import tour6 from './images/tour-6.jpeg'
+//import tour5 from './images/tour-5.jpeg'
+//import tour6 from './images/tour-6.jpeg'
 
 export const pageLinks = [
   {
