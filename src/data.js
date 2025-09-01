f4e39b5e0fec1705c740c823dfa0070b8b46861e
York Yu <n11453559@gmail.com>
2025-09-01 21:36:09 +1000

    Refactor Footer, Services, and Tours components to use data from external sources for dynamic rendering

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+
 export const pageLinks = [
   {
     id: 1,
@@ -36,5 +43,69 @@ export const socialLinks = [
     id: 3,
     href: 'https://www.squarespace.com',
     icon: 'fab fa-squarespace',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: '6 days',
+    price: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'indonesia',
+    duration: '11 days',
+    price: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'hong kong',
+    duration: '8 days',
+    price: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'kenya',
+    duration: '20 days',
+    price: 3300,
   },
 ]
