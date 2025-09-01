a96ea3d5f1c8bdbdf1ff6440873a8250f283c2d9
York Yu <n11453559@gmail.com>
2025-09-01 21:36:51 +1000

    Fix copyright date display in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   )
